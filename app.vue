43fbcdf87408a7cb5b8ae638d922dd708c6c33de
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-02-05 11:00:27 -0500

    2 files modified, 1 added

--- app.vue
+++ app.vue
@@ -1,25 +1,46 @@
 <template>
-  <div class="bg-slate-900 min-h-full h-screen text-slate-100 p-4">
-    <h1 class="text-3xl font-bold py-2">CREATE MARKDOWN LINKS</h1>
+  <div class="bg-slate-900 min-h-full h-screen text-slate-100">
+    <theHeader />
     <div>
-      <span class="text-lg">Paste your links</span>
+      <label class="text-lg" for="theMessage">Paste your links</label>
       <br />
       <textarea
         v-model="message"
         placeholder="add multiple lines"
         class="w-full bg-stone-700"
+        name="theMessage"
       >
       </textarea>
       <div class="flex items-center mb-4">
-    <input id="default-checkbox" type="checkbox" value="" v-model="bulletpoints" @onclick="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="default-checkbox" class="ml-2 text-sm font-medium ">Bullet points</label>
-</div>
+        <input
+          id="default-checkbox"
+          type="checkbox"
+          value=""
+          v-model="bulletpoints"
+          @onclick="bulletpoints != bulletpoints"
+          class="
+            w-4
+            h-4
+            text-blue-600
+            bg-gray-100
+            border-gray-300
+            rounded
+            focus:ring-blue-500
+            dark:focus:ring-blue-600 dark:ring-offset-gray-800
+            focus:ring-2
+            dark:bg-gray-700 dark:border-gray-600
+          "
+        />
+        <label for="default-checkbox" class="ml-2 text-sm font-medium"
+          >Bullet points</label
+        >
+      </div>
       <div
         v-if="message"
         v-for="word in message.split('\n')"
         style="white-space: pre-line"
       >
-      
+        <span v-if="bulletpoints">- </span>
         <span class="text-stone-500">[</span>
         {{ word.replace(/(^\w+:|^)\/\//, '') }}
         <span class="text-stone-500">]</span>
---
+++ components/theHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl font-bold py-6 px-2 text-center">
+    <span class="text-teal-400">[</span>
+    MAKE MARKDOWN LINKS
+    <span class="text-teal-400">]</span>
+  </h1>
+</template>
