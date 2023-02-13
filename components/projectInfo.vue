a94c0a74c16d8517db122002cc6362ecfb2611a5
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-02-12 23:32:29 -0500

    1 file modified

--- components/projectInfo.vue
+++ components/projectInfo.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="grid grid-cols-8 bg-stone-900 mx-auto py-4 my-8">
+  <div class="grid grid-cols-8 bg-stone-900 mx-auto py-4 my-8 shadow-sm">
     <div class="col-span-1 flex justify-center items-center mx-auto p-2">
       <Icon
         icon="material-symbols:code-blocks-outline-sharp"
         style="font-size: 38px"
       />
     </div>
-    <div class="col-span-7 flex my-auto pr-2">
+    <div class="col-span-7 flex my-auto pr-2 text-lg">
       Use this to format a list of links for markdown.
     </div>
   </div>
